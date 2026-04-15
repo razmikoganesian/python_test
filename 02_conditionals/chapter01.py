af8b7afb89083eb328b6f63f999063e379f01d08
Razmik <oganesyan.ra@gmail.com>
2026-04-15 19:30:56 +0400

    Conditions 1.0

--- 02_conditionals/chapter01.py
+++ 02_conditionals/chapter01.py
@@ -1,4 +1,13 @@
-import arrow
+kettle_boiled = False
+if kettle_boiled:
+    print(f"Kettle Done! Time to make some tea")
+else:
+    print(f"Water is not boiled!")
 
-time = arrow.utcnow()
-print(time)
+snack = input("Enter your preferred snack: ").lower()
+
+if snack == "cookies" or snack == "chipsi":
+    print(f"Order confirmed with {snack}")
+else:
+    print("No way man")
+
