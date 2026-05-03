a11d985b6b4b44129102aca2ac35ae1831cb5bb5
Razmik <oganesyan.ra@gmail.com>
2026-05-03 18:50:42 +0400

    Challenges v1.2

---
+++ challenges/day6.py
@@ -0,0 +1,15 @@
+import datetime
+
+entry = input("what did you learned today?  ").strip()
+rating = input("Please rat your productivity for today  ").strip()
+
+now = datetime.datetime.now()
+date_str  = now.strftime("%Y-%m-%d - $I:%M %p")
+
+journal_entry = f"\n 📅 {date_str}\n{entry}"
+if rating:
+    journal_entry += f"\n Procutivityrating is {rating}/5 ⭐️"
+journal_entry += "\n" + "-" * 50
+
+with open("Learning journal.txt", "a", encoding="utf_8") as f: # a - append mode
+    f.write(journal_entry)
