ceb417a743e6b3de0b4aeb0119e2db3ef88eb6e2
Razmik <oganesyan.ra@gmail.com>
2026-06-07 11:52:18 +0400

    rename of files

---
+++ 15_data_science/day10/day10_recomendation_engine1.py
@@ -0,0 +1,44 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import streamlit as st
+
+df = pd.read_csv("books.csv")
+df["title"] = df["title"].str.strip()
+df["desription"] = df["desription"].fillna("")
+
+vectorizer = TfidfVectorizer(stop_words="english")
+tfidf_matrix = vectorizer.fit_transform(df["desription"])
+
+cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
+
+indices = pd.Series(df.index, index=df["title"].str.lower()).drop_duplicates()
+
+
+def get_recommendations(title, df, cosine_sim, indices):
+    title = title.strip().lower()
+
+    if title not in indices.index:
+        return f"{title} not found in data base"
+    idx = indices.loc[title]
+    if isinstance(idx, pd.Series):
+        idx = idx.iloc[0]
+    idx = indices[title]
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)[1:6]
+    book_indices = [i[0] for i in sim_scores]
+    return df[["title", "author"]].iloc[book_indices]
+
+
+st.title("Book recomendation engine")
+st.write("Enter a book Title and get similar recomndation")
+
+select_book = st.text_input("Book title:  ")
+
+if select_book:
+    results = get_recommendations(select_book, df, cosine_sim, indices)
+
+    if isinstance(results, pd.DataFrame):
+        st.table(results)
+    else:
+        st.warning(results)
