5e35f1b69fe6499de67cce5fea79d583e78d58a5
Razmik <oganesyan.ra@gmail.com>
2026-06-10 17:59:53 +0400

    async v1.1

--- 18_async/03_async_three.py
+++ 18_async/03_async_three.py
@@ -1,1 +1,17 @@
-#
+import asyncio
+import aiohttp
+
+
+async def fetch_url(session, url):
+    async with session.get(url) as response:
+        print(f"Fetch {url} status", response.status)
+
+
+async def main():
+    urls = ["https://httpbin.org/delay/2"] * 3
+    async with aiohttp.ClientSession() as session:
+        tasks = [fetch_url(session, url) for url in urls]
+        await asyncio.gather(*tasks)
+
+
+asyncio.run(main())
