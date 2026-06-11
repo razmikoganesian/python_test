1e8295157ba02a744f3ba54bdd08f72e35d1648b
Razmik <oganesyan.ra@gmail.com>
2026-06-11 15:41:24 +0400

    deadlock/race condition

---
+++ 18_async/07_daemon_no.py
@@ -0,0 +1,14 @@
+import threading
+import time
+
+
+def monitor_tea_temp():
+    while True:
+        print(f"Monitoring tea temparature...")
+        time.sleep(3)
+
+
+t = threading.Thread(target=monitor_tea_temp)
+t.start()
+
+print("Main programm done!")
